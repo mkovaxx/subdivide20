008d86bc8c22375040b1668776fd6db16a0ef9d9
Mate Kovacs <mkovaxx@gmail.com>
2025-05-17 18:31:57 +0900

    revert unnecessary changes

--- qvlib/src/QvDebugError.cpp
+++ qvlib/src/QvDebugError.cpp
@@ -1,16 +1,14 @@
 #include <QvDebugError.hpp>
 #include <QvString.hpp>
-#include <cstdarg>
-#include <cstdio>
+#include <stdarg.h>
 
 void QvDebugError::post(const char* methodName, const char* formatString...) {
     char buf[10000];
     va_list ap;
 
     va_start(ap, formatString);
-    vsnprintf(buf, sizeof(buf), formatString, ap);
+    vsprintf(buf, formatString, ap);
     va_end(ap);
-    buf[sizeof(buf) - 1] = '\0';  // Ensure null-termination
 
     fprintf(stderr, "VRML error in %s(): %s\n", methodName, buf);
 }
