18eb9e92a0a3f1f5da8de001d4d185be26009b85
Mate Kovacs <mkovaxx@gmail.com>
2025-05-17 18:39:32 +0900

    revert

--- qvlib/src/QvReadError.cpp
+++ qvlib/src/QvReadError.cpp
@@ -1,17 +1,15 @@
 #include <QvInput.hpp>
 #include <QvReadError.hpp>
 #include <QvString.hpp>
-#include <cstdarg>
-#include <cstdio>
+#include <stdarg.h>
 
 void QvReadError::post(const QvInput* in, const char* formatString...) {
     char buf[10000];
     va_list ap;
 
     va_start(ap, formatString);
-    vsnprintf(buf, sizeof(buf), formatString, ap);
+    vsprintf(buf, formatString, ap);
     va_end(ap);
-    buf[sizeof(buf) - 1] = '\0';  // Ensure null-termination
 
     QvString locstr;
     in->getLocationString(locstr);
